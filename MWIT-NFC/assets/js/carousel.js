b46e0921c2513ca2ae6cc5d6f76fe812d735c6d6
Euro34 <133733253+Euro34@users.noreply.github.com>
2025-08-05 00:08:21 +0700

    update photos

---
+++ MWIT-NFC/assets/js/carousel.js
@@ -0,0 +1,54 @@
+let swiper = null;
+
+function Photos_click() {
+    document.getElementById('image-overlay').style.display = 'block';
+    document.getElementById("mediaModal").classList.add("active");
+
+    fetch(photos_path)
+        .then(res => res.json())
+        .then(data => {
+            const wrapper = document.getElementById("carouselContent");
+            wrapper.innerHTML = ""; // Clear previous slides
+
+            data.forEach(item => {
+                const slide = document.createElement("div");
+                slide.className = "swiper-slide";
+
+                if (item.type === "image") {
+                slide.innerHTML = `<img src="${item.src}" alt="">`;
+                } else if (item.type === "video") {
+                slide.innerHTML = `
+                    <video loop autoplay muted playsinline>
+                    <source src="${item.src}" type="video/mp4">
+                    Your browser does not support video.
+                    </video>`;
+                }
+
+                wrapper.appendChild(slide);
+            });
+
+            if (swiper) {
+                swiper.destroy(true, true);
+            }
+
+            swiper = new Swiper('.swiper', {
+                loop: true,
+                autoplay: {
+                    delay: 4000,
+                },
+                slidesPerView: 1,
+                centeredSlides: true,
+                speed: 800,
+            });
+        });
+}
+
+function closeModal() {
+    document.getElementById('image-overlay').style.display = 'none';
+    document.getElementById("mediaModal").classList.remove("active");
+    if (swiper) {
+        swiper.autoplay.stop();
+        swiper.destroy(true, true); // clean up here too
+        swiper = null;
+    }
+}
